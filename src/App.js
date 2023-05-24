c4d47102dd6240d0da19b70467bc4eb9015c3dbf
CharlesLZY <charlesliangzhiyuan@gmail.com>
2023-05-24 00:13:40 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import bgImage from './bg.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src={bgImage} alt="background"  className="Background"/>
+    </div>
+  );
+}
+
+export default App;
